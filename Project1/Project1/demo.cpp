a08ab2052b42773b73e27775a404cf0cc741d19d
xxxx <yyyy@bbktel.com>
2023-01-18 18:08:05 +0800

    更新

--- Project1/Project1/demo.cpp
+++ Project1/Project1/demo.cpp
@@ -1,1 +1,13 @@
 #include <string.h>
+#include <stdio.h>
+int main(){
+	int arr[10] = { 0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+	for (i = 0; i < sz; i++){
+		arr[i] = i + 1;
+		printf("%d\n", arr[i]);
+	}
+	
+	return 0;
+}
